5b45cacff70e6c28152f6a365c6d0e3162d5d7eb
Guillaume Sere <guillaumesere@hotmail.fr>
2023-02-28 19:00:30 +0100

    add title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Picasso from '../images/Picasso.gif'
 
 const Header = () => {
-  return (
-    <div className='header'>
-      <img src={Picasso} alt="gif" />
-    </div>
-  )
+    return (
+        <div className='header'>
+            <h1>GALERIE</h1>
+            <img src={Picasso} alt="gif" />
+            <h1>IA</h1>
+        </div>
+    )
 }
 
 export default Header
