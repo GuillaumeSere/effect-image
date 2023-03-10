8405678e8bdd6c345ac8006ed666139f9ecd75b8
Guillaume Sere <guillaumesere@hotmail.fr>
2023-03-10 20:15:59 +0100

    add function handleshow

--- src/components/CardHorizontal.jsx
+++ src/components/CardHorizontal.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import data from '../data/data'
 
 const CardHorizontal = () => {
+
+    const [show, setShow] = useState(false);
+
     return (
         <div className='horizontal-container'>
-            {data.sort(() => Math.random() - 0.5).map(({ id, image, title }) => {
+            {data.sort(() => Math.random() - 0.5).map(({ id, image }) => {
+                const handleShow = () => {
+                    setShow(true);
+                    window.open(image, '_blank');
+                };
                 return (
                     <div className='horizontal-card' key={id}>
                         <div className="horizontal-card-img">
-                            <img src={image} alt="illustartion" />
+                            <img src={image} alt="illustartion" onClick={handleShow} openWindow={true} />
                         </div>
                     </div>
                 )
